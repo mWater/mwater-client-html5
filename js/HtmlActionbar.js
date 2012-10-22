644aed8101075b87c7fe1bb609ad13e4f289e46f
Clayton Grassick <development@mwater.co>
2012-10-22 16:35:28 -0400

    Actionbar perfect.

--- js/HtmlActionbar.js
+++ js/HtmlActionbar.js
@@ -9,12 +9,15 @@ var HtmlActionbar = function(container, opts) {
 	var menuCallback; 
 	
 	var html = '<div class="navbar navbar-inverse' + (opts.fixedTop ? ' navbar-fixed-top' : '') + '">';
+    html+='<a class="btn btn-navbar dropdown-toggle pull-right" data-toggle="dropdown" id="navbar_dropdown_button"><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></a>';
+    html+='<ul class="dropdown-menu pull-right" id="navbar_items_dropdown" role="menu"></ul>'
     html+='<ul class="nav pull-right" id="navbar_items"></ul>';
 	html+='<div class="navbar-inner" style="padding-left: 10px; text-align:center">';
     html+='<div id="navbar_back" class="button bordered back pull-left">';
     html+='<span class="pointer"></span>';
     html+='<div class="content">Back</div></div>'; 
     html+='<span class="brand" id="navbar_brand" style="float:none; color:white"><span id="navbar_title"></span></span></div></div>';
+    
 	var navbar = $(html);
             
 	container.prepend(navbar);
@@ -30,14 +33,23 @@ var HtmlActionbar = function(container, opts) {
 		menuCallback = callback;
 		
 		navbar.find("#navbar_items").html('');
+		navbar.find("#navbar_items_dropdown").html('');
+		navbar.find("#navbar_dropdown_button").hide();
+		
 		_.each(items, function(item) {
 			nitem = $('<li><a href="#">' + (item.icon ? '<img height="20" width="20" src="' + item.icon + '">' : '') + item.title + '</a></li>')
 			nitem.click(function() {
+				navbar.find('[data-toggle="dropdown"]').parent().removeClass('open');
 				callback(item.id);
 				return false;
 			});
 
-			navbar.find("#navbar_items").append('<li class="divider-vertical"></li>').append(nitem);
+			if (item.ifRoom) 
+				navbar.find("#navbar_items").append('<li class="divider-vertical"></li>').append(nitem);
+			else {
+				navbar.find("#navbar_dropdown_button").show();
+				navbar.find("#navbar_items_dropdown").append(nitem);
+			}
 		});
 	};
 
