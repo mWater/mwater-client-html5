3f03807fb7c6ecca83222af482da34de1527e4a0
Clayton Grassick <development@mwater.co>
2012-11-01 10:27:26 -0400

    Better error handling

--- js/Pager.js
+++ js/Pager.js
@@ -4,218 +4,217 @@
  * context: Object whose members will be inserted into each page before create is called
  * container: DOM element wrapped in jQuery where to display pages */
 Pager = function(container, context, actionbar) {
-	this.container = container;
-	this.context = context;
+    this.container = container;
+    this.context = context;
 
-	var that = this;
-	var pressedElem;
+    var that = this;
+    var pressedElem;
 
-	// Make links and anything with class 'tappable' act on taps
-	container.on("mousedown touchstart", "a,button,.tappable", function(e) {
-		$(this).addClass("pressed");
-		pressedElem = this;
-	});
+    // Make links and anything with class 'tappable' act on taps
+    container.on("mousedown touchstart", "a,button,.tappable", function(e) {
+        $(this).addClass("pressed");
+        pressedElem = this;
+    });
 
-	container.on("mouseup touchleave touchend touchmove touchcancel scroll", function(e) {
-		if (pressedElem) {
-			$(pressedElem).removeClass("pressed");
-			pressedElem = null;
-		}
-	});
+    container.on("mouseup touchleave touchend touchmove touchcancel scroll", function(e) {
+        if (pressedElem) {
+            $(pressedElem).removeClass("pressed");
+            pressedElem = null;
+        }
+    });
 
-	// Make checkboxes tappable
-	container.on("tap", ".checkbox", function(e) {
-		$(this).toggleClass("checked");
-		$(this).triggerHandler("checked");
-	});
-	
-	// Prevent links from launching
-	container.on("click", "a", function(e) {
-		return false;
-	})
+    // Make checkboxes tappable
+    container.on("tap", ".checkbox", function(e) {
+        $(this).toggleClass("checked");
+        $(this).triggerHandler("checked");
+    });
 
-	this.stack = new Array();
-	this.state = new Array();
-
-	this.activatePage = function() {
-		var page = _.last(this.stack);
-
-		// Create actionbar if present
-		if (actionbar) {
-			actionbar.menu(page.actionbarMenu || [], function(id) {
-				if (id == "home")
-					that.closePage();
-				else if (page.actionbarMenuClick)
-					page.actionbarMenuClick(id);
-			});
-
-			if (page.actionbarTitle)
-				actionbar.title(page.actionbarTitle);
-			else
-				actionbar.title(null);
-
-			actionbar.up(this.stack.length > 1, this.stack.length > 1 ? this.stack[this.stack.length - 2].actionbarTitle : null);
-		}
-
-		if (page.activate)
-			page.activate();
-	}
-
-	// Try to load from pages global
-	this.onLoad = function(name, success, error) {
-		if (pages && pages[name])
-			success(pages[name]);
-		else
-			error();
-	}
-
-	// Loads a page on top of existing pages. Pass name and args array
-	// to create it. Default action is to look in global "pages"
-	// for constructor with name
-	this.loadPage = function(name, args, callback, onlyCreate) {
-		// Create page
-		this.onLoad(name, function(constructor) {
-			if (!onlyCreate) {
-				// Hide current page
-				if (that.stack.length > 0) {
-					if (_.last(that.stack).deactivate)
-						_.last(that.stack).deactivate();
-					_.last(that.stack).$el.detach();
-				}
-			}
-
-			// Call constructor
-			var page = Object.create(constructor.prototype);
-			constructor.apply(page, args);
-
-			// Set pager and context
-			page.pager = that;
-			_.extend(page, context);
-
-			// Add new page
-			that.stack.push(page);
-			that.state.push({
-				name : name,
-				args : args
-			});
-			that.onStateChanged(that.state);
-
-			function activatePage() {
-				that.container.html('');
-				that.container.append(page.$el);
-
-				// Scroll to top
-				container.parent().scrollTop(0)
-
-				that.activatePage(page);
-			}
-
-			// Create page element and convenience selector
-			page.$el = $('<div id="page"></div>');
-			page.$ = function(sel) {
-				return page.$el.find(sel);
-			};
-
-			// Create page
-			if (page.create)
-				page.create(function() {
-					if (!onlyCreate)
-						activatePage();
-					if (callback)
-						callback(page);
-				});
-			else if (!onlyCreate) {
-				activatePage();
-				if (callback)
-					callback(page);
-			}
-		}, function(error) {
-			console.error("Unable to load page. " + JSON.stringify(error));
-			alert("Unable to load page. " + error);
-		});
-	}
-
-	/* Closes the top page, optionally replacing it with another page */
-	this.closePage = function(replaceWith, args) {
-		// Ignore if no pages would be left
-		if (!replaceWith && this.stack.length <= 1)
-			return;
-
-		// If at least one page to close
-		if (this.stack.length > 0) {
-			var page = this.stack.pop();
-			this.state.pop();
-			this.onStateChanged(that.state);
-
-			// Deactivate current page and destroy
-			if (page.deactivate)
-				page.deactivate();
-			if (page.destroy)
-				page.destroy();
-
-			page.$el.remove();
-		}
-
-		if (replaceWith)
-			this.loadPage(replaceWith, args);
-		else if (this.stack.length > 0) {
-			// Activate current page
-			this.container.append(_.last(this.stack).$el);
-			this.activatePage();
-		}
-	}
-
-	// Sets the state
-	this.setState = function(newstate) {
-		// Create each page before last
-		if (newstate.length > 1) {
-			// Create page, but do not attach
-			this.loadPage(_.first(newstate).name, _.first(newstate).args, function() {
-				that.setState(_.rest(newstate));
-			}, true);
-		} else if (newstate.length > 0)
-			this.loadPage(newstate[0].name, newstate[0].args);
-	}
+    // Prevent links from launching
+    container.on("click", "a", function(e) {
+        return false;
+    })
 
 
-	this.onStateChanged = function(state) {
-	}
+    this.stack = new Array();
+    this.state = new Array();
 
-}
+    this.activatePage = function() {
+        var page = _.last(this.stack);
+
+        // Create actionbar if present
+        if (actionbar) {
+            actionbar.menu(page.actionbarMenu || [], function(id) {
+                if (id == "home")
+                    that.closePage();
+                else if (page.actionbarMenuClick)
+                    page.actionbarMenuClick(id);
+            });
+
+            if (page.actionbarTitle)
+                actionbar.title(page.actionbarTitle);
+            else
+                actionbar.title(null);
+
+            actionbar.up(this.stack.length > 1, this.stack.length > 1 ? this.stack[this.stack.length - 2].actionbarTitle : null);
+        }
+
+        if (page.activate)
+            page.activate();
+    };
+
+    // Try to load from pages global
+    this.onLoad = function(name, success, error) {
+        if (pages && pages[name])
+            success(pages[name]);
+        else
+            error();
+    };
+
+    // Loads a page on top of existing pages. Pass name and args array
+    // to create it. Default action is to look in global "pages"
+    // for constructor with name
+    this.loadPage = function(name, args, callback, onlyCreate) {
+        // Create page
+        this.onLoad(name, function(constructor) {
+            if (!onlyCreate) {
+                // Hide current page
+                if (that.stack.length > 0) {
+                    if (_.last(that.stack).deactivate)
+                        _.last(that.stack).deactivate();
+                    _.last(that.stack).$el.detach();
+                }
+            }
+
+            // Call constructor
+            var page = Object.create(constructor.prototype);
+            constructor.apply(page, args);
+
+            // Set pager and context
+            page.pager = that;
+            _.extend(page, context);
+
+            // Add new page
+            that.stack.push(page);
+            that.state.push({
+                name : name,
+                args : args
+            });
+            that.onStateChanged(that.state);
+
+            function activatePage() {
+                that.container.html('');
+                that.container.append(page.$el);
+
+                // Scroll to top
+                container.parent().scrollTop(0)
+
+                that.activatePage(page);
+            }
+
+            // Create page element and convenience selector
+            page.$el = $('<div id="page"></div>');
+            page.$ = function(sel) {
+                return page.$el.find(sel);
+            };
+
+            // Create page
+            if (page.create)
+                page.create(function() {
+                    if (!onlyCreate)
+                        activatePage();
+                    if (callback)
+                        callback(page);
+                });
+            else if (!onlyCreate) {
+                activatePage();
+                if (callback)
+                    callback(page);
+            }
+        }, function(error) {
+            console.error("Unable to load page. " + JSON.stringify(error));
+            alert("Unable to load page. " + error);
+        });
+    };
+
+    /* Closes the top page, optionally replacing it with another page */
+    this.closePage = function(replaceWith, args) {
+        // Ignore if no pages would be left
+        if (!replaceWith && this.stack.length <= 1)
+            return;
+
+        // If at least one page to close
+        if (this.stack.length > 0) {
+            var page = this.stack.pop();
+            this.state.pop();
+            this.onStateChanged(that.state);
+
+            // Deactivate current page and destroy
+            if (page.deactivate)
+                page.deactivate();
+            if (page.destroy)
+                page.destroy();
+
+            page.$el.remove();
+        }
+
+        if (replaceWith)
+            this.loadPage(replaceWith, args);
+        else if (this.stack.length > 0) {
+            // Activate current page
+            this.container.append(_.last(this.stack).$el);
+            this.activatePage();
+        }
+    };
+
+    // Sets the state
+    this.setState = function(newstate) {
+        // Create each page before last
+        if (newstate.length > 1) {
+            // Create page, but do not attach
+            this.loadPage(_.first(newstate).name, _.first(newstate).args, function() {
+                that.setState(_.rest(newstate));
+            }, true);
+        } else if (newstate.length > 0)
+            this.loadPage(newstate[0].name, newstate[0].args);
+    };
+
+
+    this.onStateChanged = function(state) {
+    };
+};
 
 Pager.makeTappable = function(table, ontap) {
-	table.on("tap", "tr", function(e) {
-		ontap(this)
-	});
+    table.on("tap", "tr", function(e) {
+        ontap(this)
+    });
 
-	var pressedElem;
+    var pressedElem;
 
-	table.on("mousedown touchstart", "tr", function(e) {
-		$(this).addClass("pressed");
-		pressedElem = this;
-	});
+    table.on("mousedown touchstart", "tr", function(e) {
+        $(this).addClass("pressed");
+        pressedElem = this;
+    });
 
-	table.on("mouseup touchleave touchend touchmove touchcancel mousemove scroll", function(e) {
-		if (pressedElem) {
-			$(pressedElem).removeClass("pressed");
-			_.delay(function() {
-				$(pressedElem).removeClass("pressed");
-				pressedElem = null;
-			}, 100);
-		}
-	});
-
-}
+    table.on("mouseup touchleave touchend touchmove touchcancel mousemove scroll", function(e) {
+        if (pressedElem) {
+            $(pressedElem).removeClass("pressed");
+            _.delay(function() {
+                $(pressedElem).removeClass("pressed");
+                pressedElem = null;
+            }, 100);
+        }
+    });
+};
 
 Pager.makeSearchable = function(table, searchbar) {
-	searchbar.on("input change", function(ev) {
-		var q = this.value;
-		table.find("tr").each(function(i) {
-			src = sources[i]
-			if (src.name.toLowerCase().indexOf(q.toLowerCase()) == -1)
-				$(this).hide();
-			else
-				$(this).show();
-		});
-	});
-}
+    searchbar.on("input change", function(ev) {
+        var q = this.value;
+        table.find("tr").each(function(i) {
+            src = sources[i]
+            if (src.name.toLowerCase().indexOf(q.toLowerCase()) == -1)
+                $(this).hide();
+            else
+                $(this).show();
+        });
+    });
+};
--- js/ProblemReporter.js
+++ js/ProblemReporter.js
@@ -1,60 +1,74 @@
 function ProblemReporter(baseUrl, version, getClientUid) {
-	var history = [];
-	var that = this;
+    var history = [];
+    var that = this;
 
-	function capture(func) {
-		var old = console[func];
-		console[func] = function(arg) {
-			history.push(arg);
-			if (history.length > 200)
-				history.splice(0, 20);
-			old.call(console, arg);
-		}
+    function capture(func) {
+        var old = console[func];
+        console[func] = function(arg) {
+            history.push(arg);
+            if (history.length > 200)
+                history.splice(0, 20);
+            old.call(console, arg);
+        }
 
-	}
+    }
 
-	capture("log");
-	capture("warn");
-	capture("error");
+    capture("log");
+    capture("warn");
+    capture("error");
 
-	function getLog() {
-		var log = "";
-		_.each(history, function(item) {
-			log += String(item) + "\r\n";
-		});
-		return log;
-	}
+    function getLog() {
+        var log = "";
+        _.each(history, function(item) {
+            log += String(item) + "\r\n";
+        });
+        return log;
+    }
 
-	this.reportProblem = function(desc) {
-		// Create log string
-		var log = getLog();
-		
+
+    this.reportProblem = function(desc) {
+        // Create log string
+        var log = getLog();
+
         console.log("Reporting problem...");
-        
-		$.post(baseUrl + "problem_reports", {
-			clientuid : getClientUid(),
-			version : version,
-			log : log,
-			desc : desc
-		});
-	}; 
-	
-	var debouncedReportProblem = _.debounce(this.reportProblem, 5000, true);
 
-	var oldError = console.error;
-	console.error = function(arg) {
-		oldError(arg);
-		
-		debouncedReportProblem("Internal Error");
-	}
+        $.post(baseUrl + "problem_reports", {
+            clientuid : getClientUid(),
+            version : version,
+            log : log,
+            desc : desc
+        });
+    };
+
+    // Capture error logs
+    var debouncedReportProblem = _.debounce(this.reportProblem, 5000, true);
+
+    var oldConsoleError = console.error;
+    console.error = function(arg) {
+        oldConsoleError(arg);
+
+        debouncedReportProblem("Console Error");
+    };
+
+    // Capture window.onerror
+    var oldWindowOnError = window.onerror;
+    window.onerror = function(errorMsg, url, lineNumber) {
+        console.log("window.onerror:" + errorMsg + ":" + url + ":" + lineNumber);
+        that.reportProblem("Internal Error");
+        
+        // Put up alert instead of old action
+        alert("Internal Error\n" + errorMsg + "\n" + url + ":" + lineNumber);
+        //if (oldWindowOnError)
+        //    oldWindowOnError(errorMsg, url, lineNumber);
+    };
 }
 
 ProblemReporter.register = function(baseUrl, version, getClientUid) {
-	if (!ProblemReporter.instances)
-		ProblemReporter.instances = {}
-		
-	if (ProblemReporter.instances[baseUrl])
-		return;
-	
-	new ProblemReporter(baseUrl, version, getClientUid);
+    if (!ProblemReporter.instances)
+        ProblemReporter.instances = {}
+
+    if (ProblemReporter.instances[baseUrl])
+        return;
+
+    new ProblemReporter(baseUrl, version, getClientUid);
 }
